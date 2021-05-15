3497226f18758a2e41b94c74131869cfee98917a
MaxLlovera <61042419+MaxLlovera@users.noreply.github.com>
2021-05-16 00:28:00 +0200

    Cosas chido
    Cosas chido

--- Game/Source/Quests.cpp
+++ Game/Source/Quests.cpp
@@ -66,6 +66,8 @@ bool Quests::Start()
 		questsTex = app->tex->Load("Assets/Textures/quests.png");
 		markTex = app->tex->Load("Assets/Textures/quest_done.png");
 		journalTex = app->tex->Load("Assets/Textures/tasks.png");
+		questFx = app->audio->LoadFx("Assets/Audio/Fx/open_quest.wav");
+		questDoneFx = app->audio->LoadFx("Assets/Audio/Fx/quest_done.wav");
 		questsIsOpen = false;
 
 	}
@@ -80,16 +82,19 @@ bool Quests::Update(float dt)
 	{
 		questsIsOpen = true;
 		app->hud->bagIsOpen = false;
+		app->audio->PlayFx(questFx, 0);
 		app->shop->open = false;
 
 	} else if (app->input->GetKey(SDL_SCANCODE_Q) == KEY_DOWN && questsIsOpen || pad.b == true)
 	{
 		questsIsOpen = false;
+		app->audio->PlayFx(questFx, 0);
 	}
 
-	if (app->sceneBattle->enemy1Dead && app->sceneBattle->enemy2Dead && app->sceneBattle->enemy3Dead || app->input->GetKey(SDL_SCANCODE_K) == KEY_DOWN)
+	if (app->sceneBattle->enemy1Dead && app->sceneBattle->enemy2Dead && app->sceneBattle->enemy3Dead)
 	{
 		quest1Done = true;
+
 	}
 	
 	if (app->sceneDungeon->active)
@@ -195,6 +200,11 @@ bool Quests::PostUpdate()
 
 	if (quest1Done)
 	{
+		if (counterQuest1 < 2)
+		{
+			app->audio->PlayFx(questDoneFx, 0);
+		}
+
 		if (counterQuest1 < 200) 
 		{
 			app->render->DrawRectangle({ 0, 580, 1280, 140 }, 0, 0, 0, 220, true, false);
@@ -206,6 +216,10 @@ bool Quests::PostUpdate()
 	
 	if (quest2Done)
 	{
+		if (counterQuest2 < 2)
+		{
+			app->audio->PlayFx(questDoneFx, 0);
+		}
 		if (counterQuest2 < 200)
 		{
 			app->render->DrawRectangle({ 0, 580, 1280, 140 }, 0, 0, 0, 220, true, false);
@@ -216,6 +230,10 @@ bool Quests::PostUpdate()
 	}
 	if (quest3Done)
 	{
+		if (counterQuest3 < 2)
+		{
+			app->audio->PlayFx(questDoneFx, 0);
+		}
 		if (counterQuest3 < 200)
 		{
 			app->render->DrawRectangle({ 0, 580, 1280, 140 }, 0, 0, 0, 220, true, false);
@@ -296,7 +314,7 @@ void Quests::drawMark2()
 	SrcR.h = 720;
 
 	DestR.x = 400;
-	DestR.y = 426;
+	DestR.y = 326;
 	DestR.w = 40;
 	DestR.h = 40;
 	SDL_RenderCopy(app->render->renderer, markTex, &SrcR, &DestR);
